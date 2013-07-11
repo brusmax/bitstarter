ab01ed932b52f444694818f454ca55d97fe404e4
Brusmax <enrique@thinkblack.net>
2013-07-11 01:17:10 -0500

    buffering fs.readFileSync

--- web.js
+++ web.js
@@ -4,13 +4,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  buf = new Buffer(data);
-  buf.write("some string", 0, "ascii");
-  response.send(data);
-});
-});
+ var buf = fs.readFileSync('./index.html','utf8');
+	response.send(buf.toString('utf8',0,buf.length));
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
